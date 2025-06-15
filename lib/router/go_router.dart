02c1ded0ca8e34e1e7ac4a815127a22b35f46133
aemamonov <mamonov.aleks.e@gmail.com>
2025-06-16 02:14:43 +0300

    update imports

--- lib/router/go_router.dart
+++ lib/router/go_router.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_builder_problem/router/typed_router/home_routes.dart';
-import 'package:go_router_builder_problem/router/typed_router/root_routes.dart';
+import 'package:go_router_builder_problem/router/typed_router/home_routes.dart'
+    show $homeRoute;
+import 'package:go_router_builder_problem/router/typed_router/root_routes.dart'
+    show $rootRoutes;
 import 'package:go_router_builder_problem/features/main/presentation/main_page.dart';
 import 'package:go_router_builder_problem/features/splash/presentation/splash_page.dart';
 
